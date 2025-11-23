9bd44feda0ae7cb9cbc93a93d79404bfa3ddb283
yeo-drake <lenermbgamer345@gmail.com>
2025-11-23 17:49:01 -0500

    feat: Transplante de buckets

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCkXrNpdR7brWaM_mrtzJNZvKHsvenXLgA",
-  authDomain: "allinkawsayzampo.firebaseapp.com",
-  projectId: "allinkawsayzampo",
-  storageBucket: "allinkawsayzampo.firebasestorage.app",
-  messagingSenderId: "320303224766",
-  appId: "1:320303224766:web:0ea45911ad441cd4c68bef"
+  apiKey: "AIzaSyA_LLOI-xaw2Hn_Z29Uf-3ZpK88QmgPgMw",
+  authDomain: "allin-kawsay-zampo-app.firebaseapp.com",
+  projectId: "allin-kawsay-zampo-app",
+  storageBucket: "allin-kawsay-zampo-app.firebasestorage.app",
+  messagingSenderId: "162132540652",
+  appId: "1:162132540652:web:256af1a6b2fe9b38b5b9c9"
 };
 
 // Exportamos la configuración para que otros archivos la puedan usar
